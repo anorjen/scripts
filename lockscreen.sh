be95d5cf6dc2299a20792fcbe8f246665eb6d987
Anorjen <1qwert147@rambler.ru>
2020-01-29 16:25:31 +0300

    add screen lock scripts

---
+++ lockscreen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+# Take a screenshot
+scrot -o /tmp/screen_locked.png
+
+# Pixellate it 10x
+# mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+
+# Blur
+mogrify -channel RGBA -blur 0x6 /tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -e -i /tmp/screen_locked.png
+
+# Turn the screen off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off
