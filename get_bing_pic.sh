3b7700a301015a1bd282d6ffeda12c1ced1ac1ce
Anorjen <1qwert147@rambler.ru>
2020-08-07 13:41:17 +0300

    get bing picture or set random

--- get_bing_pic.sh
+++ get_bing_pic.sh
@@ -7,20 +7,25 @@
 
 sleep 2
 
-SITE="https://www.bing.com/"
+SITE="https://www.bing.com"
 
 WALLS_DIR="$HOME/Pictures/backgrounds"
 WALLPAPER="$WALLS_DIR/wallpaper_$(date +"%y-%m-%d").jpg"
 
 if [[ ! -s $WALLPAPER ]]
 then
-	PIC=`curl $SITE | grep -o -e 'href="\(\/[a-zA-Z0-9.?=_]\+.jpg\)' | cut -c 7-`
+	PIC=`curl $SITE | grep -o -e 'href="\(\/[a-zA-Z0-9.?=_]\+1920x1080.jpg\)' | cut -c 7-`
 	URL="$SITE$PIC"
 	STATUS=`(wget -O $WALLPAPER -- $URL &>/dev/null && echo OK) || echo Fail`
+
 	if [[ "$STATUS" = "OK" ]]
 	then
 		cp $WALLPAPER $WALLS_DIR/login-background.jpg
 		mogrify -channel RGBA -blur 0x6 $WALLS_DIR/login-background.jpg
+		feh --bg-fill $WALLPAPER
+	else
+		feh --randomize --bg-fill ~/Pictures/backgrounds/*
 	fi
-fi	
-feh --bg-fill $WALLPAPER
+else
+	feh --bg-fill $WALLPAPER
+fi
