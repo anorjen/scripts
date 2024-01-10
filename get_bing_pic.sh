d6651ccfbc38f0ac2b6f2bdd792ba7e0c9226ec9
Anorjen <1qwert147@rambler.ru>
2024-01-10 17:00:11 +0300

    update get_bing_pic - add UHD wallpaper

--- get_bing_pic.sh
+++ get_bing_pic.sh
@@ -7,25 +7,86 @@
 
 sleep 2
 
+SET_UHD=1
+
 SITE="https://www.bing.com"
 
 WALLS_DIR="$HOME/Pictures/backgrounds"
-WALLPAPER="$WALLS_DIR/wallpaper_$(date +"%y-%m-%d").jpg"
 
-if [[ ! -s $WALLPAPER ]]
+WALLS_DIR_FHD="$WALLS_DIR/FHD"
+WALLPAPER_FHD="$WALLS_DIR_FHD/wallpaper_$(date +"%y-%m-%d").jpg"
+
+WALLS_DIR_UHD="$WALLS_DIR/UHD"
+WALLPAPER_UHD="$WALLS_DIR_UHD/wallpaper_UHD_$(date +"%y-%m-%d").jpg"
+
+if [[ $SET_UHD -eq 1 ]]
 then
-	PIC=`curl $SITE | grep -o -e 'href="\(\/[a-zA-Z0-9.?=_]\+1920x1080.jpg\)' | cut -c 7-`
-	URL="$SITE$PIC"
-	STATUS=`(wget -O $WALLPAPER -- $URL &>/dev/null && echo OK) || echo Fail`
+	SET_DIR=$WALLS_DIR_UHD
+	WALLPAPER=$WALLPAPER_UHD
+else
+	SET_DIR=$WALLS_DIR_FHD
+	WALLPAPER=$WALLPAPER_FHD
+fi
 
-	if [[ "$STATUS" = "OK" ]]
+function isImage {
+	possibleImage=$1
+	
+	status=`file $possibleImage |grep -c "image"`
+	echo $status
+}
+
+function downloadWallpapers {
+	pic=`curl -A "Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36" $SITE | grep -o -e '"Wallpaper":"\(\/[a-zA-Z0-9.?=_]\+1920x1200.jpg\)' | cut -c 14-`
+	pic_fhd=`echo $pic | sed 's/1920x1200/1920x1080/'`
+	pic_uhd=`echo $pic | sed 's/1920x1200/UHD/'`
+
+	url="$SITE$pic_fhd"
+	echo $url
+	url_uhd="$SITE$pic_uhd"
+	echo $url_uhd
+
+	status_fhd=$( downloadWallpaper $WALLPAPER_FHD $url)
+	echo "FHD wallpaper download: " $status_fhd
+	status_uhd=$( downloadWallpaper $WALLPAPER_UHD $url_uhd)
+	echo "UHD wallpaper download: " $status_uhd
+}
+
+function downloadWallpaper {
+	wallpaper=$1
+	url=$2
+
+	status=`(wget -O $wallpaper -- $url &>/dev/null && echo OK) || echo Fail`
+	
+	if [[ "$status" != "OK" || ! -s $wallpaper ]]
+	then
+		echo "Fail"
+		return
+	fi
+	
+	isWallpaper=$( isImage $wallpaper)
+	if [[ isWallpaper -eq 1 ]]
+	then
+		echo "OK"
+	else
+		rm -f $wallpaper
+		echo "Fail"
+	fi
+}
+
+function setWallpaper {
+	if [[ -s $WALLPAPER ]]
 	then
 		cp $WALLPAPER $WALLS_DIR/login-background.jpg
 		mogrify -channel RGBA -blur 0x6 $WALLS_DIR/login-background.jpg
 		feh --bg-fill $WALLPAPER
 	else
-		feh --randomize --bg-fill ~/Pictures/backgrounds/*
+		feh --randomize --bg-fill $SET_DIR/*
 	fi
-else
-	feh --bg-fill $WALLPAPER
+}
+
+if [[ ! -s $WALLPAPER ]]
+then
+	downloadWallpapers
 fi
+
+setWallpaper
